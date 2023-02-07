d73099dbd06af4cc3ffa946fb8218ad314d41a6b
Latesun <latesun.lee@gmail.com>
2023-02-08 00:06:09 +0800

    feat(leetcode): no.22 generate parenthesis

---
+++ src/leetcode/lc0022_generate_parenthesis.rs
@@ -0,0 +1,47 @@
+struct Solution {}
+
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        if n < 1 {
+            return vec![];
+        }
+
+        let mut parentheses: Vec<String> = Vec::new();
+        Self::generate(0, 0, n, "".to_string(), &mut parentheses);
+        parentheses
+    }
+
+    fn generate(left: i32, right: i32, n: i32, parenthesis: String, parentheses: &mut Vec<String>) {
+        if left == n && right == n {
+            parentheses.push(parenthesis);
+            return;
+        }
+
+        if left < n {
+            let mut parenthesis = parenthesis.clone();
+            parenthesis.push('(');
+            Self::generate(left + 1, right, n, parenthesis, parentheses);
+        }
+
+        if right < left {
+            let mut parenthesis = parenthesis.clone();
+            parenthesis.push(')');
+            Self::generate(left, right + 1, n, parenthesis, parentheses);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generate_parenthesis() {
+        assert_eq!(Solution::generate_parenthesis(1), vec!["()"]);
+        assert_eq!(Solution::generate_parenthesis(2), vec!["(())", "()()"]);
+        assert_eq!(
+            Solution::generate_parenthesis(3),
+            vec!["((()))", "(()())", "(())()", "()(())", "()()()"]
+        );
+    }
+}
